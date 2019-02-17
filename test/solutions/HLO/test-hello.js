4a72d65546e30b217294d886b42cb783969be477
sbx_user1060 <sbx_user1060@ip-10-129-224-206.eu-west-2.compute.internal>
2019-02-17 08:19:26 +0000

    Sun Feb 17 08:19:26 UTC 2019

---
+++ test/solutions/HLO/test-hello.js
@@ -0,0 +1,7 @@
+const hello = require("../../../lib/solutions/HLO/hello");
+
+describe("hello unit tests", function() {
+  it("should say hello to the world", function(){
+      hello("mark");
+  });
+});
