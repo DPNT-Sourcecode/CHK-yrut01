0ecfcf0b1c0b40b3dff00de73df0c4247a0204ac
sbx_user1060 <sbx_user1060@ip-10-129-224-206.eu-west-2.compute.internal>
2019-02-17 08:00:15 +0000

    Sun Feb 17 08:00:15 UTC 2019

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -1,8 +1,8 @@
-var mocha = require('mocha')
-var describe = mocha.describe
-var it = mocha.it
-var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+var mocha = require("mocha");
+var describe = mocha.describe;
+var it = mocha.it;
+var assert = require("assert");
+const sum = require("../../../lib/solutions/SUM/sum");
 
 /*
  - param[0] = a positive integer between 0-100
@@ -10,23 +10,27 @@ const sum = require('../../../lib/solutions/SUM/sum');
  - @return = an Integer representing the sum of the two numbers
  */
 
-describe('SUM challenge: adding two numbers', function() {
-	
-	it('should only accept 0-100 for param[0]', function(){
-		assert.throws(function(){
-			sum(101,2);
-		});
-	});
+describe("SUM challenge: adding two numbers", function() {
+  it("should reject invalid inputs", function() {
+    assert.throws(function() {
+      sum(undefined, 20);
+    });
+  });
 
-	it('should reject invalid input for param[1]', function(){
-		assert.throws(function(){
-			sum(22,-1000);
-		});
-	});
-	
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
-	});
+  it("should only accept 0-100 for param[0]", function() {
+    assert.throws(function() {
+      sum(101, 2);
+    });
+  });
 
+  it("should reject invalid input for param[1]", function() {
+    assert.throws(function() {
+      sum(22, -1000);
+    });
+  });
 
+  it("should return 3, which is the sum of 1 and 2", function() {
+    assert.equal(sum(1, 2), 3);
+  });
 });
+
