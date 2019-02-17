5dc367c598775a6f64096daae7467b7476ddb8b0
sbx_user1060 <sbx_user1060@ip-10-129-224-206.eu-west-2.compute.internal>
2019-02-17 07:54:15 +0000

    Sun Feb 17 07:54:15 UTC 2019

--- test/solutions/SUM/test-sum.js
+++ test/solutions/SUM/test-sum.js
@@ -4,8 +4,23 @@ var it = mocha.it
 var assert = require('assert');
 const sum = require('../../../lib/solutions/SUM/sum');
 
+/*
+ - param[0] = a positive integer between 0-100
+ - param[1] = a positive integer between 0-100
+ - @return = an Integer representing the sum of the two numbers
+ */
+
 describe('SUM challenge: adding two numbers', function() {
+	
+	it('should only accept 0-100 for param[0]', function(){
+		assert.throws(function(){
+			sum(101,2);
+		});
+	});
+	
 	it('should return 3, which is the sum of 1 and 2', function() {
 	    assert.equal(sum(1, 2), 3);
 	});
+
+
 });
