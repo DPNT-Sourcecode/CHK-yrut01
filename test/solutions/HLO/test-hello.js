f5b7e47391a4d2bf35f7ddae6a010f9bd7ef28c4
sbx_user1060 <sbx_user1060@ip-10-129-224-206.eu-west-2.compute.internal>
2019-02-17 08:24:01 +0000

    Sun Feb 17 08:24:01 UTC 2019

--- test/solutions/HLO/test-hello.js
+++ test/solutions/HLO/test-hello.js
@@ -1,7 +1,13 @@
 const hello = require("../../../lib/solutions/HLO/hello");
+const assert = require("assert");
 
 describe("hello unit tests", function() {
-  it("should say hello to the world", function(){
-      hello("mark");
+  it("should say hello to the world", function() {
+    const name = "mark";
+    const result = hello(name);
+
+    assert.equal(result, `Hello, ${name}!`);
+
   });
 });
+
