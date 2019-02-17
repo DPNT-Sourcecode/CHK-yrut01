cedcddbd44cd5b2e8e44672fe788052dc1378bfe
sbx_user1059 <sbx_user1059@ip-10-129-153-131.eu-west-2.compute.internal>
2019-02-17 20:45:54 +0000

    Sun Feb 17 20:45:54 UTC 2019

---
+++ test/solutions/CHK/test-checkout.js
@@ -0,0 +1,7 @@
+const checkout = require("../../../lib/solutions/CHK/checkout");
+
+describe.only("checkout tests", () => {
+  it("should implement the checkout method", ()=> {
+      const result = checkout("SOMETEXT");
+  });
+});
